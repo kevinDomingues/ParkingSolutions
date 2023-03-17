1c088f374fc4e81f2f2464b2e5f406cd00337041
kevinDomingues <kevincarvas@gmail.com>
2023-03-17 20:08:45 +0000

    feat - Enable ParkScreen to get a list of parks and use a custom card Item

--- app/src/main/java/pt/ipvc/parkingsolutions/presentation/parks/ParksScreen.kt
+++ app/src/main/java/pt/ipvc/parkingsolutions/presentation/parks/ParksScreen.kt
@@ -1,31 +1,29 @@
 package pt.ipvc.parkingsolutions.presentation.parks
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.rememberAsyncImagePainter
+import pt.ipvc.parkingsolutions.ui.components.CardItem
 
 @Composable
 fun ParksScreen(
     parksViewModel: ParksViewModel = hiltViewModel()
 ){
-    val getData = parksViewModel.state.value
+    val parks = parksViewModel.state.value
 
-    Column(
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = rememberAsyncImagePainter(model = getData.image), contentDescription = null)
-        Text(text = getData.name)
-        Text(text = getData.location)
-        Text(text = getData.nFreeSpaces.toString())
+        items(parks){each ->
+            CardItem(name = each.name, location = each.location, nFreeSpaces = each.nFreeSpaces, image = each.image)
+        }
     }
 
 }
---
+++ app/src/main/java/pt/ipvc/parkingsolutions/ui/components/CardItem.kt
@@ -0,0 +1,80 @@
+package pt.ipvc.parkingsolutions.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun CardItem(name: String, location: String, nFreeSpaces: Int, image: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .height(280.dp),
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = Color.White,
+        elevation = 10.dp
+    ){
+        Column {
+            Image(
+                painter = rememberAsyncImagePainter(model = image),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = name,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = location,
+                    modifier = Modifier.padding(top = 8.dp),
+                    color = Color.LightGray,
+                    fontSize = 13.sp,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row() {
+                    Text(
+                        text = "Free Spaces: ",
+                        modifier = Modifier.padding(top = 8.dp),
+                        color = Color.LightGray,
+                        fontSize = 13.sp,
+                    )
+                    Text(
+                        text = nFreeSpaces.toString(),
+                        modifier = Modifier.padding(top = 8.dp),
+                        color = Color.Black,
+                        fontSize = 13.sp,
+                    )
+                }
+
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    CardItem(name = "Name", location = "Location", nFreeSpaces = 2, image = "https://radiogeice.com/wp-content/uploads/2020/04/parque-encosta-do-elevador-viana-do-castelo.jpg")
+}
