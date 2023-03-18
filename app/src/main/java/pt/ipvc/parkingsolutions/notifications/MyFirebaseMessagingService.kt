cad01266cf0a56d380ab8faf0e77ce7dd75c7e6a
kevinDomingues <kevincarvas@gmail.com>
2023-03-18 01:13:39 +0000

    feat - Use default layout for notifications

--- app/src/main/java/pt/ipvc/parkingsolutions/notifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/pt/ipvc/parkingsolutions/notifications/MyFirebaseMessagingService.kt
@@ -1,13 +1,11 @@
 package pt.ipvc.parkingsolutions.notifications
 
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -24,16 +22,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
   }
 
-  @SuppressLint("RemoteViewLayout")
-  fun getRemoteView(title: String, message: String): RemoteViews {
-      val remoteView = RemoteViews("pt.ipvc.parkingsolutions", R.layout.notification)
-
-      remoteView.setTextViewText(R.id.title, title)
-      remoteView.setTextViewText(R.id.message, message)
-
-    return remoteView
-  }
-
   fun generateNotifications(title: String, message: String) {
 
     val intent = Intent(this, MainActivity::class.java)
@@ -44,12 +32,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     //channel id, channel name
     var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
       .setSmallIcon(R.drawable.img)
+      .setContentTitle(title)
+      .setContentText(message)
       .setAutoCancel(true)
       .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
       .setOnlyAlertOnce(true)
       .setContentIntent(pendingIntent)
-
-    builder = builder.setContent(getRemoteView(title, message))
 
     val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
